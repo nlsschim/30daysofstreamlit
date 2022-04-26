d9a7f865fb932fae949d4c1d69f86e6fe2efb13b
Nels Schimek <nlsschim@uw.edu>
2022-04-26 13:43:34 -0700

    day 12

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,18 @@
 import streamlit as st
 
-st.header('st.multiselect')
+st.header('st.checkbox')
 
-options = st.multiselect(
-	'What are your favorite colors',
-	['Green', 'Yellow', 'Red', 'Blue'],
-	['Yellow', 'Red']
-)
+st.write('What would you like to order?')
 
-st.write('You selected:', options)
+icecream = st.checkbox('Ice Cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+	st.write('Great! here is some more :icecream:')
+
+if coffee:
+	st.write("Okay, here is some coffee :coffee:")
+
+if cola:
+	st.write('Here you go 🥤')
