40af47183e774f5398322289d53e2a31cc73802c
Nels Schimek <nlsschim@uw.edu>
2022-04-27 15:16:34 -0700

    first commit for seirsplus web app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,39 @@
 import streamlit as st
-import requests
+import seirsplus
 
-st.title('🏀 Bored API app')
+import numpy as np
+import itertools
+# seirsplus libraries
+from seirsplus.models.preconfig_disease_models import *
+from seirsplus.networks import *
+from seirsplus import utils
+from seirsplus.sim_loops import *
+from seirsplus import scenarios
 
-st.sidebar.header('Input')
-selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+st.header('SEIRS+ Wrappers')
 
-suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
-json_data = requests.get(suggested_activity_url)
-suggested_activity = json_data.json()
+option = st.selectbox(
+    'Which wrapper would you like to run?',
+    ('', 'Community Scenario', 'Intervention Scenario', 'Primary School Scenario', 'Secondary School Scenario')
+)
 
-c1, c2 = st.columns(2)
-with c1:
-  with st.expander('About this app'):
-    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
-with c2:
-  with st.expander('JSON data'):
-    st.write(suggested_activity)
 
-st.header('Suggested activity')
-st.info(suggested_activity['activity'])
+if option == 'Community Scenario':
+    community_results, community_caselogs = scenarios.run_SARSCoV2_community_scenario()
+    st.write('Community results:')
+    st.write(community_results)
 
-col1, col2, col3 = st.columns(3)
-with col1:
-  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
-with col2:
-  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
-with col3:
-  st.metric(label='Price', value=suggested_activity['price'], delta='')
+if option == 'Intervention Scenario':
+    interventions_results, interventions_caselogs = scenarios.run_SARSCoV2_interventions_scenario()
+    st.write('Interventions results')
+    st.write(interventions_results)
+
+if option == 'Primary School Scenario':
+    primary_school_results, primary_school_caselogs = scenarios.run_SARSCoV2_primary_school_scenario()
+    st.write('Primary School results')
+    st.write(primary_school_results)
+
+if option == 'Secondary School Scenario':
+    secondary_school_results, secondary_school_caselogs = scenarios.run_SARSCoV2_secondary_school_scenario()
+    st.write('Secondary School results')
+    st.write(secondary_school_results)
