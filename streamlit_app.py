0d64ed20354b3b3985ea99a611df44b3730512ef
Nels Schimek <nlsschim@uw.edu>
2022-04-27 14:03:48 -0700

    day 26

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,30 @@
 import streamlit as st
+import requests
 
-st.title('st.form')
+st.title('🏀 Bored API app')
 
-# Full example of using the with notation
-st.header('1. Example of using `with` notation')
-st.subheader('Coffee machine')
+st.sidebar.header('Input')
+selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
 
-with st.form('my_form'):
-    st.write('**Order your coffee**')
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
 
-    # Input widgets
-    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
-    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
-    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
-    serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
-    milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
-    owncup_val = st.checkbox('Bring own cup')
+c1, c2 = st.columns(2)
+with c1:
+  with st.expander('About this app'):
+    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+with c2:
+  with st.expander('JSON data'):
+    st.write(suggested_activity)
 
-    # Every form must have a submit button
-    submitted = st.form_submit_button('Submit')
+st.header('Suggested activity')
+st.info(suggested_activity['activity'])
 
-if submitted:
-    st.markdown(f'''
-        ☕ You have ordered:
-        - Coffee bean: `{coffee_bean_val}`
-        - Coffee roast: `{coffee_roast_val}`
-        - Brewing: `{brewing_val}`
-        - Serving type: `{serving_type_val}`
-        - Milk: `{milk_val}`
-        - Bring own cup: `{owncup_val}`
-        ''')
-else:
-    st.write('☝️ Place your order!')
-
-
-# Short example of using an object notation
-st.header('2. Example of object notation')
-
-form = st.form('my_form_2')
-selected_val = form.slider('Select a value')
-form.form_submit_button('Submit')
-
-st.write('Selected value: ', selected_val)
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+with col2:
+  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
+with col3:
+  st.metric(label='Price', value=suggested_activity['price'], delta='')
